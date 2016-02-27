0280de8c2343587881297235705b36579e0a5b51
Gabriel Mojica <niko.mojica@live.com>
2016-02-27 20:21:21 +1100

    feat: Add 5 markers to map

--- js/map.js
+++ js/map.js
@@ -1,6 +1,32 @@
+
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-	    center: {lat: -34.397, lng: 150.644},
-	    zoom: 8
-    });
+
+	var locations = {
+		pyrmont: new google.maps.LatLng(-33.8665, 151.1956),
+		artGallery: new google.maps.LatLng(-33.868791, 151.217413),
+		lunaPark: new google.maps.LatLng(-33.847677, 151.209699),
+		torongaZoo: new google.maps.LatLng(-33.839200, 151.240775),
+		operaHouse: new google.maps.LatLng(-33.856657, 151.215270)
+	};
+
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: locations.operaHouse,
+	    zoom: 14
+	});
+
+	for (var location in locations) {
+		new google.maps.Marker({
+			position: locations[location],
+			map: map,
+			title: location
+		});
+		console.log(location);
+	}
+
+	new google.maps.Marker({
+		position: new google.maps.LatLng(),
+		map: map,
+		title: 'miko'
+	});
+
 }
