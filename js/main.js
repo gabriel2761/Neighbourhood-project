4844e12b97f7c670ef4c9b983e01a83ef4b5c1cb
Gabriel Mojica <niko.mojica@live.com>
2016-02-29 22:10:23 +1100

    feat: Filter pane and markers based on search

--- js/main.js
+++ js/main.js
@@ -1,49 +1,56 @@
-
-var Location = function(name, lat, lng) {
-	this.name = name;
-	this.lat = lat;
-	this.lng = lng;
-};
-
-var locations = [
-	new Location('Pyrmont', -33.8665, 151.1956),
-	new Location('Art Gallery of NSW' ,-33.868791, 151.217413),
-	new Location('Luna Park', -33.847677, 151.209699),
-	new Location('Toronga Zoo', -33.839200, 151.240775),
-	new Location('Opera House', -33.856657, 151.215270)
-];
-
 function initMap() {
 
-	map = new google.maps.Map(document.getElementById('map'), {
+	var map = new google.maps.Map(document.getElementById('map'), {
 		center: new google.maps.LatLng(-33.856657, 151.215270),
 	    zoom: 13
 	});
 
-	locations.forEach(function(location) {
-		new google.maps.Marker({
-			position: new google.maps.LatLng(location.lat, location.lng),
+	var AppViewModel = function() {
+		this.keyword = ko.observable('');
+		this.markers = ko.observableArray([]);
+	};
+
+	AppViewModel.prototype.addMarker = function(title, lat, lng) {
+		this.markers().push(new google.maps.Marker({
+			position: new google.maps.LatLng(lat, lng),
 			map: map,
-			title: location.name
+			title: title
+		}));
+	};
+
+	AppViewModel.prototype.setMarkers = function(markers) {
+		markers().forEach(function(marker) {
+			marker.setMap(map);
 		});
-	});
+	};
 
-}
+	AppViewModel.prototype.clearMarkers = function() {
+		this.markers().forEach(function(marker) {
+			marker.setMap(null);
+		});
+	};
 
-var AppViewModel = function() {
-	this.locations = ko.observableArray(locations);
-	this.keyword = ko.observable('Pyrmont');
-
-	this.search = function() {
-		var result = ko.observableArray([]);
-		for (var i = 0; i < locations.length; i++) {
-			if (locations[i].name.toLowerCase().indexOf(this.keyword().toLowerCase()) != -1) {
-				result.push(locations[i]);
+	AppViewModel.prototype.search = function() {
+		var results = ko.observableArray([]);
+		for (var i = 0; i < this.markers().length; i++) {
+			if (this.markers()[i].title.toLowerCase().indexOf(this.keyword().toLowerCase()) !== -1) {
+				results().push(this.markers()[i]);
 			}
 		}
-		return result;
+		this.clearMarkers();
+		this.setMarkers(results);
+		return results;
 	};
-};
 
-ko.applyBindings(new AppViewModel());
+	var viewModel = new AppViewModel();
+
+	viewModel.addMarker('Pyrmont', -33.8665, 151.1956);
+	viewModel.addMarker('Art Gallery of NSW' ,-33.868791, 151.217413);
+	viewModel.addMarker('Luna Park', -33.847677, 151.209699);
+	viewModel.addMarker('Toronga Zoo', -33.839200, 151.240775);
+	viewModel.addMarker('Opera House', -33.856657, 151.215270);
+
+	ko.applyBindings(viewModel);
+}
+
 
