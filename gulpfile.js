0bf9fd75e23cee05f535756e9e572830ec0073b2
Gabriel Mojica <niko.mojica@live.com>
2016-07-16 01:07:12 +1000

    chore: Create gulp build system

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,24 @@
 var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
+var inline = require('gulp-inline-source');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
 
-gulp.task('default', function() {
+gulp.task('default', ['minify', 'compress']);
 
+gulp.task('minify', function() {
+  return gulp.src('src/index.html')
+    .pipe(inline())
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('compress', function(cb) {
+    pump([
+        gulp.src('src/js/main.js'),
+        uglify(),
+        gulp.dest('dist/js/')
+    ],
+    cb
+    );
 });
